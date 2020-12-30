c8d15d220f07cdb86d162b5197a091ac20607164
Oliver Otchere <Oliverotchere4@gmail.com>
2020-12-30 14:15:29 +0000

    added countries js

--- js/home.js
+++ js/home.js
@@ -1,6 +1,34 @@
 const switcher = document.querySelector('.switcher')
 const ion_icon = document.querySelector('.moon ion-icon')
 const body = document.querySelector('body')
+const filter_box = document.querySelector('.filter_box')
+const region_category = document.querySelector('.category')
+const countries_cont = document.querySelector('.countries')
+
+// UI class
+class Card {
+    constructor(card_obj) {
+        this.country = card_obj
+    }
+    createHTML() {
+        const html = `<div class="card">
+                    <div class="flag">
+                        <img src="${this.country.flag}" alt="">
+                    </div>
+                    <div class="card_info">
+                        <h2><a href="/detail.html?country=oliver">${this.country.name}</a> </h2>
+                        <h3>Population : <span>${this.country.population}</span> </h3>
+                        <h3>Region: <span>${this.country.region}</span> </h3>
+                        <h3>Capital: <span>${this.country.capital}</span> </h3>
+                    </div>
+                </div>`
+        return html
+    }
+    injectCard() {
+        const hmtl = this.createHTML()
+        countries_cont.innerHTML += hmtl
+    }
+}
 
 // functions
 function changeMood(e) {
@@ -14,4 +42,42 @@ function changeMood(e) {
     }
 }
 
+function changeRegion(e) {
+    if (region_category.style.display == 'block') {
+        region_category.style.display = 'none'
+    } else {
+        region_category.style.display = 'block'
+    }
+}
+
+function chooseRegion(e) {
+    if (e.target.tagName === 'LI') {
+        console.log(e.target.textContent)
+    }
+    e.stopPropagation()
+}
+// 'https://restcountries.eu/rest/v2/all'
+async function fetchCountries() {
+    response = await fetch('https://restcountries.eu/rest/v2/all')
+    data = await response.json()
+    return data
+}
+
+const country = fetchCountries()
+
+country.then(data => {
+    const country_data = data.slice(0, 6)
+    country_data.forEach(country => {
+        const countryCard = new Card(country)
+        countryCard.injectCard()
+    })
+}).catch(error => {
+    console.log(error)
+})
+
+// event listeners
 switcher.addEventListener('click', changeMood)
+
+filter_box.addEventListener('click', changeRegion)
+
+region_category.addEventListener('click', chooseRegion)
