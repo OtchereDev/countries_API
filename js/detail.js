f042d174956fb875cae840593b45b0d85d000d67
Oliver Otchere <oliver@hubtel.com>
2022-07-24 18:04:37 +0000

    updated api ref

--- js/detail.js
+++ js/detail.js
@@ -1,117 +1,115 @@
-const country_name = document.querySelector('.name')
-const country_native = document.querySelector('.native span')
-const country_pop = document.querySelector('.pop span')
-const country_region = document.querySelector('.region span')
-const country_subreg = document.querySelector('.sub span')
-const country_cap = document.querySelector('.cap span')
-const country_domain = document.querySelector('.domain span')
-const country_curr = document.querySelector('.curr span')
-const country_lan = document.querySelector('.lan span')
-const country_flag = document.querySelector('.flag')
-const switcher = document.querySelector('.switcher')
-const ion_icon = document.querySelector('.moon ion-icon')
-const body = document.querySelector('body')
-const btn_cont = document.querySelector('.border_btn')
-const back_btn = document.querySelector('.back_btn')
-const country_det = document.querySelector('.country_det')
+const country_name = document.querySelector(".name");
+const country_native = document.querySelector(".native span");
+const country_pop = document.querySelector(".pop span");
+const country_region = document.querySelector(".region span");
+const country_subreg = document.querySelector(".sub span");
+const country_cap = document.querySelector(".cap span");
+const country_domain = document.querySelector(".domain span");
+const country_curr = document.querySelector(".curr span");
+const country_lan = document.querySelector(".lan span");
+const country_flag = document.querySelector(".flag");
+const switcher = document.querySelector(".switcher");
+const ion_icon = document.querySelector(".moon ion-icon");
+const body = document.querySelector("body");
+const btn_cont = document.querySelector(".border_btn");
+const back_btn = document.querySelector(".back_btn");
+const country_det = document.querySelector(".country_det");
 
 // functions
 function changeMood(e) {
-    if (body.classList.contains('light_mode')) {
-        body.classList.remove('light_mode')
-        ion_icon.setAttribute('name', 'moon')
-    } else {
-        body.classList.add('light_mode')
-        ion_icon.setAttribute('name', 'moon-outline')
-
-    }
+  if (body.classList.contains("light_mode")) {
+    body.classList.remove("light_mode");
+    ion_icon.setAttribute("name", "moon");
+  } else {
+    body.classList.add("light_mode");
+    ion_icon.setAttribute("name", "moon-outline");
+  }
 }
 async function getBorderCountry(border) {
-    response = await fetch(`https://restcountries.eu/rest/v2/alpha/${border}`)
-    data = await response.json()
-    return data
+  response = await fetch(`https://restcountries.eu/rest/v2/alpha/${border}`);
+  data = await response.json();
+  return data;
 }
 
 async function fetchCountry(name) {
-    response = await fetch(`https://restcountries.eu/rest/v2/name/${name}`)
-    data = await response.json()
-    const status = response.ok
-    return { data, status }
+  response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+  data = await response.json();
+  const status = response.ok;
+  return { data, status };
 }
 
 function btnMaker(country) {
-    const html = `<button>${country}</button>`
-    return html
+  const html = `<button>${country}</button>`;
+  return html;
 }
 
 function backHome(e) {
-    window.location.replace(`index.html`)
+  window.location.replace(`index.html`);
 }
 
 function redirectBorder(e) {
-    if (e.target.tagName === 'BUTTON') {
-        console.log(e.target.textContent)
-        window.location.replace(`detail.html?country=${e.target.textContent}`)
-    }
+  if (e.target.tagName === "BUTTON") {
+    window.location.replace(`detail.html?country=${e.target.textContent}`);
+  }
 }
 
 // get particular country
 const urlParam = new URLSearchParams(window.location.search);
-const myParam = urlParam.get('country')
+const myParam = urlParam.get("country");
 
-const country_found = fetchCountry(myParam)
+const country_found = fetchCountry(myParam);
 
-country_found.then(data => {
+country_found.then((data) => {
+  if (data.status) {
+    const datas = data.data[0];
 
-    if (data.status) {
-        const datas = data.data[0]
+    country_flag.setAttribute("src", datas.flags.svg);
+    country_name.textContent = datas.name.official;
+    country_native.textContent = datas.nativeName;
+    country_pop.textContent = datas.population;
+    country_region.textContent = datas.region;
+    country_subreg.textContent = datas.subregion;
+    country_cap.textContent = datas.capital;
+    country_domain.textContent = datas?.topLevelDomain?.[0];
+    Object.keys(datas.languages)?.forEach((currency) => {
+      if (country_curr.textContent) {
+        country_curr.textContent += `, ${datas.languages[currency]}`;
+      } else {
+        country_curr.textContent = `${datas.languages[currency]}`;
+      }
+    });
+    // datas?.currencies?.forEach((curr) => {
+    // });
+    Object.keys(datas.languages).forEach((lan) => {
+      if (country_lan.textContent) {
+        country_lan.textContent += `, ${datas.languages[lan]}`;
+      } else {
+        country_lan.textContent = `${datas.languages[lan]}`;
+      }
+    });
 
-        country_flag.setAttribute('src', datas.flag);
-        country_name.textContent = datas.name;
-        country_native.textContent = datas.nativeName;
-        country_pop.textContent = datas.population;
-        country_region.textContent = datas.region;
-        country_subreg.textContent = datas.subregion;
-        country_cap.textContent = datas.capital;
-        country_domain.textContent = datas.topLevelDomain[0];
-        datas.currencies.forEach(curr => {
-            if (country_curr.textContent) {
-                country_curr.textContent += `, ${curr.name}`
-            } else {
-                country_curr.textContent = `${curr.name}`
-            }
-        })
-        datas.languages.forEach(lan => {
-            if (country_lan.textContent) {
-                country_lan.textContent += `, ${lan.name}`
-            } else {
-                country_lan.textContent = `${lan.name}`
-            }
-        })
+    country_lan.textContent = datas.languages[Object.keys(datas.languages)[0]];
 
-        if (datas.borders.length) {
-            datas.borders.forEach(border => {
-                getBorderCountry(border).then(data => {
-                    const btn = btnMaker(data.name)
-                    btn_cont.innerHTML += btn
-                })
-            })
-        } else {
-            btn_cont.innerHTML = '<h3>This county has no neigbouring countries</h3>'
-            console.log('no')
-        }
-
+    if (datas.borders?.length) {
+      datas.borders.forEach((border) => {
+        getBorderCountry(border).then((data) => {
+          const btn = btnMaker(data.name);
+          btn_cont.innerHTML += btn;
+        });
+      });
     } else {
-        country_det.innerHTML = '<h1>No country data for country with this name</h1>'
-        country_det.style.width = '100%'
-        country_det.style.justifyContent = 'center'
-        country_det.style.textAlign = 'center'
-
+      btn_cont.innerHTML = "<h3>This county has no neigbouring countries</h3>";
     }
-
-})
+  } else {
+    country_det.innerHTML =
+      "<h1>No country data for country with this name</h1>";
+    country_det.style.width = "100%";
+    country_det.style.justifyContent = "center";
+    country_det.style.textAlign = "center";
+  }
+});
 
 // event listeners
-switcher.addEventListener('click', changeMood)
-back_btn.addEventListener('click', backHome)
-btn_cont.addEventListener('click', redirectBorder)
+switcher.addEventListener("click", changeMood);
+back_btn.addEventListener("click", backHome);
+btn_cont.addEventListener("click", redirectBorder);
--- js/home.js
+++ js/home.js
@@ -1,174 +1,166 @@
-const switcher = document.querySelector('.switcher')
-const ion_icon = document.querySelector('.moon ion-icon')
-const body = document.querySelector('body')
-const filter_box = document.querySelector('.filter_box')
-const region_category = document.querySelector('.category')
-const countries_cont = document.querySelector('.countries')
-const load_btn = document.querySelector('.load')
-const searchInput = document.querySelector('.search_form input')
-const searchForm = document.querySelector('.search_form')
+const switcher = document.querySelector(".switcher");
+const ion_icon = document.querySelector(".moon ion-icon");
+const body = document.querySelector("body");
+const filter_box = document.querySelector(".filter_box");
+const region_category = document.querySelector(".category");
+const countries_cont = document.querySelector(".countries");
+const load_btn = document.querySelector(".load");
+const searchInput = document.querySelector(".search_form input");
+const searchForm = document.querySelector(".search_form");
 
 // UI class
 class Card {
-    constructor(card_obj) {
-        this.country = card_obj
-    }
-    createHTML() {
-        const html = `<div class="card">
+  constructor(card_obj) {
+    this.country = card_obj;
+  }
+  createHTML() {
+    const html = `<div class="card">
                     <div class="flag">
-                        <img src="${this.country.flag}" alt="">
+                        <img src="${this.country.flags.svg}" alt="">
                     </div>
                     <div class="card_info">
-                        <h2><a href="detail.html?country=${this.country.name}">${this.country.name}</a> </h2>
+                        <h2><a href="detail.html?country=${this.country.name.official}">${this.country.name.official}</a> </h2>
                         <h3>Population : <span>${this.country.population}</span> </h3>
                         <h3>Region: <span>${this.country.region}</span> </h3>
                         <h3>Capital: <span>${this.country.capital}</span> </h3>
                     </div>
-                </div>`
-        return html
-    }
-    injectCard() {
-        const hmtl = this.createHTML()
-        countries_cont.innerHTML += hmtl
-    }
+                </div>`;
+    return html;
+  }
+  injectCard() {
+    const hmtl = this.createHTML();
+    countries_cont.innerHTML += hmtl;
+  }
 }
 
 // functions
 function changeMood(e) {
-    if (body.classList.contains('light_mode')) {
-        body.classList.remove('light_mode')
-        ion_icon.setAttribute('name', 'moon')
-    } else {
-        body.classList.add('light_mode')
-        ion_icon.setAttribute('name', 'moon-outline')
-
-    }
+  if (body.classList.contains("light_mode")) {
+    body.classList.remove("light_mode");
+    ion_icon.setAttribute("name", "moon");
+  } else {
+    body.classList.add("light_mode");
+    ion_icon.setAttribute("name", "moon-outline");
+  }
 }
 
 function changeRegion(e) {
-    if (region_category.style.display == 'block') {
-        region_category.style.display = 'none'
-    } else {
-        region_category.style.display = 'block'
-    }
+  if (region_category.style.display == "block") {
+    region_category.style.display = "none";
+  } else {
+    region_category.style.display = "block";
+  }
 }
 
 function chooseRegion(e) {
-    if (e.target.tagName === 'LI') {
-        const country = fetchRegionCountry(e.target.textContent)
+  if (e.target.tagName === "LI") {
+    const country = fetchRegionCountry(e.target.textContent);
 
-        countries_cont.innerHTML = ''
+    countries_cont.innerHTML = "";
 
-        country.then(data => {
-
-            loadMore()
-            load_btn.addEventListener('click', loadMore)
-        }).catch(error => {
-            console.log(error)
-        })
-    }
-
+    country
+      .then((data) => {
+        loadMore();
+        load_btn.addEventListener("click", loadMore);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
 
 function loadMore() {
-    const start = document.querySelectorAll('.countries .card').length
-    const end = start + 6
-    const country_data = data.slice(start, end)
-    country_data.forEach(country => {
-        const countryCard = new Card(country)
-        countryCard.injectCard()
-    })
-    if (end > data.length) {
-        console.log(data.length, end)
-        load_btn.setAttribute('disabled', 'true')
-        load_btn.textContent = 'No more data'
-        load_btn.style.cursor = 'not-allowed'
-    }
+  const start = document.querySelectorAll(".countries .card").length;
+  const end = start + 6;
+  const country_data = data.slice(start, end);
+  country_data.forEach((country) => {
+    const countryCard = new Card(country);
+    countryCard.injectCard();
+  });
+  if (end > data.length) {
+    console.log(data.length, end);
+    load_btn.setAttribute("disabled", "true");
+    load_btn.textContent = "No more data";
+    load_btn.style.cursor = "not-allowed";
+  }
 }
 
 function searchCountryName(e) {
+  const country = fetchCountryByTerm(e.target.value);
 
-    const country = fetchCountryByTerm(e.target.value)
+  countries_cont.innerHTML = "";
 
-    countries_cont.innerHTML = ''
-
-    country.then(response => {
-        if (response.status) {
-            const data = response.data
-            loadMore()
-            load_btn.addEventListener('click', loadMore)
-            countries_cont.style.justifyContent = 'space-between'
-            countries_cont.style.alignItems = 'center'
-            countries_cont.style.marginBottom = 'auto'
-            load_btn.style.display = 'block'
-        } else {
-            countries_cont.innerHTML = `<h1 style='font-size:2rem;'>No country found with name "${e.target.value}"</h1>`
-            countries_cont.style.justifyContent = 'center'
-            countries_cont.style.alignItems = 'center'
-            countries_cont.style.marginBottom = '5%'
-            load_btn.style.display = 'none'
-        }
-
-    }).catch(error => {
-        console.log(error)
+  country
+    .then((response) => {
+      if (response.status) {
+        const data = response.data;
+        loadMore();
+        load_btn.addEventListener("click", loadMore);
+        countries_cont.style.justifyContent = "space-between";
+        countries_cont.style.alignItems = "center";
+        countries_cont.style.marginBottom = "auto";
+        load_btn.style.display = "block";
+      } else {
+        countries_cont.innerHTML = `<h1 style='font-size:2rem;'>No country found with name "${e.target.value}"</h1>`;
+        countries_cont.style.justifyContent = "center";
+        countries_cont.style.alignItems = "center";
+        countries_cont.style.marginBottom = "5%";
+        load_btn.style.display = "none";
+      }
     })
+    .catch((error) => {
+      console.log(error);
+    });
 }
-
-
 
 // API stuff
 async function fetchCountries() {
-    response = await fetch('https://restcountries.eu/rest/v2/all')
-    data = await response.json()
-    return data
+  response = await fetch("https://restcountries.com/v3.1/all");
+  data = await response.json();
+  console.log("xxxx:", data);
+  return data;
 }
 
-const country = fetchCountries()
+const country = fetchCountries();
 
-country.then(data => {
-
-    loadMore()
-    load_btn.addEventListener('click', loadMore)
-}).catch(error => {
-    console.log(error)
-})
+country
+  .then((data) => {
+    loadMore();
+    load_btn.addEventListener("click", loadMore);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 async function fetchRegionCountry(region) {
-    response = await fetch(`https://restcountries.eu/rest/v2/region/${region}`)
-    data = await response.json()
-    return data
+  response = await fetch(`https://restcountries.eu/rest/v2/region/${region}`);
+  data = await response.json();
+  return data;
 }
 
 async function fetchCountryByTerm(term) {
-    if (term.length) {
-        response = await fetch(`https://restcountries.eu/rest/v2/name/${term}`)
-        data = await response.json()
-        const status = response.ok
-        return { data, status }
-    } else {
+  if (term.length) {
+    response = await fetch(`https://restcountries.eu/rest/v2/name/${term}`);
+    data = await response.json();
+    const status = response.ok;
+    return { data, status };
+  } else {
+    country_data = await fetchCountries();
 
-        country_data = await fetchCountries()
-
-        return { data: country_data, status: true }
-
-
-
-
-    }
-
+    return { data: country_data, status: true };
+  }
 }
 
 // event listeners
-switcher.addEventListener('click', changeMood)
+switcher.addEventListener("click", changeMood);
 
-filter_box.addEventListener('click', changeRegion)
+filter_box.addEventListener("click", changeRegion);
 
-region_category.addEventListener('click', chooseRegion)
+region_category.addEventListener("click", chooseRegion);
 
-searchInput.addEventListener('keydown', searchCountryName)
+searchInput.addEventListener("keydown", searchCountryName);
 
-searchForm.addEventListener('submit', e => {
-    e.preventDefault()
-    searchForm.reset()
-
-})
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchForm.reset();
+});
